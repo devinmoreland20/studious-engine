cc4d7435ca91d3b605335fafa24a858cc2eac09f
devinmoreland <devinmoreland20@gmail.com>
2022-08-23 18:00:46 -0400

    f

--- variables.tf
+++ variables.tf
@@ -67,20 +67,12 @@ variable "public_sn_count" {
   default = 3
 }
 variable "public_cidrs" {
-  type = list(string)
-  default = [
-    "10.0.1.0/24",
-    "10.0.2.0/24",
-    "10.0.3.0/24"
-  ]
+  type    = list(string)
+  default = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
 }
 variable "private_cidrs" {
-  type = string
-  default = [
-    "10.0.4.0/24",
-    "10.0.5.0/24",
-    "10.0.6.0/24"
-  ]
+  type    = string
+  default = ["10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24"]
 }
 variable "instance_type" {
   type    = string
